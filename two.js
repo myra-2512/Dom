139d0958c27089a8c4dff7f3694f72c7ef83ed9c
myra-2512 <abhi.back2back@gmail.com>
2025-04-26 11:20:25 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    var a = 2
+    document.getElementById("div2").innerHTML= a*a
+}
+
+function myValidate(e) {
+    e.prevantDefault()
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const age = document.getElementById("age").value;
+    const msg = document.getElementById("message").value;
+
+    let message1 = " "
+}
